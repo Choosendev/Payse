63d0a535718f24233bfbeb0a21d1f6a94175e046
hakikicode <57021097+hakikicode@users.noreply.github.com>
2023-08-08 18:51:27 +0100

    NEXT link
    route link

--- Frontend/payse/src/app/app.js
+++ Frontend/payse/src/app/app.js
@@ -1,19 +1,17 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Signup from "./pages/sign-up/Signup";
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 
 
 function App() {
     return (
-      <BrowserRouter>
-      <Routes>
+      <ul>
+      <li>
        
-        <Route  path="/sign-up" element={<Signup />} />
-        
-      </Routes>
-      </BrowserRouter>
+        <Link  href="/sign-up" element={<Signup />} />
+        </Link>
+      </li>
+      </ul>
     );
   }
   
