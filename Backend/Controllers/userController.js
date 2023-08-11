26745d38a838fc6492f9ebfdca89d9d3e7815330
bomanaps <mercy.boma35@yahoo.com>
2023-08-11 13:53:57 +0100

    fixed model eror

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,6 +1,5 @@
 const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 
 // @desc Register new user
